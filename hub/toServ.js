25a7ff541152c4d0ae5f873fa95758b432910bb8
Eli Smith <plunkinguitar@gmail.com>
2016-10-17 17:49:49 +0000

    Removed m2x support in favor of future server

--- hub/toServ.js
+++ hub/toServ.js
@@ -1,18 +1,22 @@
 var request = require('request');
-
+var exports = module.exports = {};
 var key = "19f56759ada36266960cc37b2ee3537c";
 var device = "15df8a57671bb58f633afa646f348dbc";
-
-var streams = [];
+var url = "http://api-m2x.att.com/v2/devices/"
 
 request.get({
     headers: {
     "X-M2X-KEY": key
     },
-    uri: "http://api-m2x.att.com/v2/devices/"+device+"/streams"
+    uri: url+device+"/streams"
 }, function(err, res, body){
     body = JSON.parse(body);
-    streams = body.streams;
+    exports.streams = body.streams;
 });
 
-console.log(streams);
+
+exports.values = function(stream) {
+    request.get({
+        
+    })
+}
