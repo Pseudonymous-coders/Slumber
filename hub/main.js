888f052eedac13b2bad132f179ae6b07fa35bf00
Eli Smith <plunkinguitar@gmail.com>
2016-11-20 01:01:38 +0000

    Some awesome changes to how the udoo processes Accel from band

--- hub/main.js
+++ hub/main.js
@@ -1,8 +1,32 @@
+require('./basics');
 var nrf = require('nrfuart');
-var toServ = require('./toServ');
-var serialCom = require('./serialCom')
+//var toServ = require('./toServ');
+//var serialCom = require('./serialCom')
 
-var xmax = ymax = zmax = tempmax = VBatt = 0;
+var counter = 0;
+
+//sets calibration cap
+var countCap = 10;
+
+var temp = 0,
+    hum = 0,
+    VBatt = 0.0;
+
+// Accel variables
+var xmin = 0,
+    ymin = 0,
+    zmin = 0;
+var xrange = 0,
+    yrange = 0,
+    zrange = 0;
+var oldX = 0,
+    oldY = 0,
+    oldZ = 0;
+var x = [],
+    y = [],
+    z = [];
+var totAccel = 0;
+
 console.log("Started BLE server...");
 nrf.discoverAll(function(ble_uart){
     console.log("Scanning for devices...");
@@ -16,32 +40,50 @@ nrf.discoverAll(function(ble_uart){
             console.log("Connected to "+ devName);
         });
 
-        ble_uart.on("data", function(data) {
+                ble_uart.on("data", function(data) {
             data = data.toString();
-            if (typeof data != "undefined"){
-                counter += 1;
-                if (data[0] == "V") {
-                    VBatt = data.substr(1);
-                } else if (data.length > 5){
-                    var rawVals = {
-                        temp: data.split(";")[0],
-                        x: data.split(";")[2].substr(0,3),
-                        y: data.split(";")[2].substr(3,6),
-                        z: data.split(";")[2].substr(6,9)
-                    };
-                    xmax = (rawVals.x > xmax) ? rawVals.x : xmax;
-                    ymax = (rawVals.y > ymax) ? rawVals.y : ymax;
-                    zmax = (rawVals.z > zmax) ? rawVals.z : zmax;
-                    tempmax = (rawVals.temp > tempmax) ? rawVals.temp : tempmax;
-                    if (counter >= 20) {
-                        counter = 0;
-                        console.log("Max vals: ");
-                        console.log("X:"+xmax);
-                        console.log("Y: "+ymax);
-                        console.log("Z: "+zmax);
-                        console.log("Temp: "+tempmax);
-                        console.log("VBatt: "+VBatt);
-                        xmax = ymax = zmax = tempmax = VBatt =0;
+            if (data.split(";").length == 4){
+                if (data[0] == "A") {
+                    var mainSense = data.split(";").slice(1);
+                } else if (data[0] == "B") {
+                    var accels = data.split(";").slice(1);
+                    var curX = parseInt(accels[0]),
+                        curY = parseInt(accels[1]),
+                        curZ = parseInt(accels[2]);
+                    x.push(curX);
+                    y.push(curY);
+                    z.push(curZ);
+                    if (counter == countCap) {
+                        console.log("Finished Calibration...");
+                        xmin = Math.min.apply(null,x);
+                        ymin = Math.min.apply(null,y);
+                        zmin = Math.min.apply(null,z);
+                        xrange = Math.max.apply(null,x) - Math.min.apply(null, x);
+                        yrange = Math.max.apply(null,y) - Math.min.apply(null, y);
+                        zrange = Math.max.apply(null,z) - Math.min.apply(null, z);
+                        counter += 1;
+                    } else if (counter < countCap) {
+                        counter += 1;
+                    } else {
+                        curX = Math.round((1/50)*Math.abs(curX - xmin));
+                        curY = Math.round((1/50)*Math.abs(curY - ymin));
+                        curZ = Math.round((1/50)*Math.abs(curZ - zmin));
+                        curX = (curX > 100) ? 100 : curX;
+                        curY = (curY > 100) ? 100 : curY;
+                        curZ = (curZ > 100) ? 100 : curZ;
+
+                        var tmpX = curX,
+                            tmpY = curY,
+                            tmpZ = curZ;
+
+                        curX = (curX > oldX - 5 && curX < oldX + 5) ? 0 : curX;
+                        curY = (curY > oldY - 5 && curY < oldY + 5) ? 0 : curY;
+                        curZ = (curZ > oldZ - 5 && curZ < oldZ + 5) ? 0 : curZ;
+
+                        oldX = tmpX;
+                        oldY = tmpY;
+                        oldZ = tmpZ;
+                        totAccel = Math.max(curX, curY, curZ);
                     }
                 }
             }
